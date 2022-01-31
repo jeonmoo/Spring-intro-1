f02cbe3fa07cdf0a724dad9cc17ddef435c87df0
JeonMoo <rjm0710@naver.com>
2022-01-31 23:24:18 +0900

    DI 의존성 주입 및 JDBC와 JDBC_Template

---
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -0,0 +1,52 @@
+package hello.hellospring;
+
+import hello.hellospring.repository.JdbcMemberRepository;
+import hello.hellospring.repository.JdbcTemplateMemberRepository;
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+import hello.hellospring.service.MemberService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+// Autowired를 사용하지 않고 직접 자바코드로 주입하는 방법
+
+@Configuration
+public class SpringConfig {
+
+    private DataSource dataSource;
+
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @Bean
+    public MemberService memberService() {
+        return new MemberService(memberRepository());
+    }
+
+    @Bean
+    public MemberRepository memberRepository() {
+//        return new MemoryMemberRepository(dataSource);
+//        return new JdbcMemberRepository(dataSource);
+        return new JdbcTemplateMemberRepository(dataSource);
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
