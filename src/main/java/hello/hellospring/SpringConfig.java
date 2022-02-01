3bc800641d693b9e3590ed3667bdfff3d73fc546
JeonMoo <rjm0710@naver.com>
2022-02-01 18:17:14 +0900

    JPA : Spring Data Member Repository
    setting

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -2,35 +2,41 @@ package hello.hellospring;
 
 import hello.hellospring.repository.*;
 import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import javax.persistence.EntityManager;
 
 // Autowired를 사용하지 않고 직접 자바코드로 주입하는 방법
 
 @Configuration
 public class SpringConfig {
 
-    private EntityManager em;
+//    private EntityManager em;
 
-    public SpringConfig(EntityManager em) {
-        this.em = em;
+//    public SpringConfig(EntityManager em) {
+//        this.em = em;
+//    }
+
+    private final MemberRepository memberRepository;
+
+    @Autowired
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
 
     @Bean
     public MemberService memberService() {
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
 
-    @Bean
-    public MemberRepository memberRepository() {
+//    @Bean
+//    public MemberRepository memberRepository() {
 //        return new MemoryMemberRepository(dataSource);
 //        return new JdbcMemberRepository(dataSource);
 //        return new JdbcTemplateMemberRepository(dataSource);
-        return new JpaMemberRepository(em);
-    }
-
+//        return new JpaMemberRepository(em);
+//    }
+//
 }
 
 
